3bb0340d0fb3f2a22b5a92b16c8cb90d3df0fd1e
seungh1024 <seungh1024@naver.com>
2021-11-18 00:19:46 +0900

    [ADD] init db model review

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const config = require('../config/config')[env];
 const User = require('./user');
 const Food = require('./food');
 const Combination = require('./combination');
+const Review = require('./review');
 
 const db ={};
 const sequelize = new Sequelize(config.database,config.username,config.password,config,);
@@ -12,13 +13,16 @@ db.sequelize = sequelize;
 db.User = User;
 db.Food = Food;
 db.Combination = Combination;
+db.Reivew = Review;
 
 User.init(sequelize);
 Food.init(sequelize);
 Combination.init(sequelize);
+Review.init(sequelize);
 
 User.associate(db);
 Food.associate(db);
 Combination.associate(db);
+Review.associate(db);
 
 module.exports = db;
